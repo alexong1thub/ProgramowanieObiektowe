02a816fd19c1477e44eb268ff78984b20936ebd5
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-04-25 09:40:45 +0200

    Uncommented important line

--- src/zadanie1/LibraryTest.java
+++ src/zadanie1/LibraryTest.java
@@ -6,7 +6,7 @@ public class LibraryTest {
     public static void main(String[] args){
         Library library = new Library();
         library.addBook(new Book("Mroczna wieża", "Stephen King", 359));
-//        library.addBookToLibraryPrompt();
+        library.addBookToLibraryPrompt();
         library.addBook("Wiedźmin Ostanie życzenie", "Andrzej Sapkowski", 400);
         library.addBook("Smallville Season Eleven", " Bryan Q. Miller", 320);
         library.showBooksInLibrary();
