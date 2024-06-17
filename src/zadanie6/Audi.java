04dd0df30833c5bf0809645d8cbd6d06d5208a6f
whatanx <whatanxx@gmail.com>
2024-06-17 11:20:05 +0200

    Modified task6. Class Audi with for driving additional parameters. Its prototype.

--- src/zadanie6/Audi.java
+++ src/zadanie6/Audi.java
@@ -4,43 +4,50 @@ import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
 
-public class Audi implements Vehicle, Car{
+public class Audi implements Vehicle, Car {
     private boolean isAc = false;
     private int speedKmH = 0;
     private boolean isSpeeding = false;
     private final int maxSpeedKmH = 240;
-    private final long LOSE_SPEED_PER_SECOND = 100;
+    private double fuelLevelPercent = 100;
+    private int mileage = 0;
+    private final int SPEED_PER_SECOND = 100;
     private final static Scanner sc = new Scanner(System.in);
+
     @Override
     public void turnOnAc() {
         isAc = !isAc;
     }
 
     @Override
-    public void drive() {
-        if(speedKmH == 0 && !isSpeeding){
+    public void drive() throws InterruptedException {
+        if (speedKmH == 0 && !isSpeeding) {
             System.out.println(getClass() + " is standing");
             System.out.println("Do you want to drive?");
             String answer = sc.nextLine();
-            if(answer.equalsIgnoreCase("yes")){
+            if (answer.equalsIgnoreCase("yes")) {
                 isSpeeding = true;
-            }
-            else{
+            } else {
                 System.out.println("Cool. Keep your head up champ!");
             }
-        }
-        else{
-            System.out.println(getClass() + " is driving at speed: " + getSpeedKmH());
-        }
+        } else if (isSpeeding) {
 
+            if (speedKmH >= maxSpeedKmH) {
+                System.out.println("Car is driving at maximal speed of " + maxSpeedKmH + " km/h");
+            } else {
+                speedKmH += SPEED_PER_SECOND;
+                sleep(1000);
+                System.out.println(getClass() + " is driving at speed: " + getSpeedKmH());
+            }
+        }
     }
 
     @Override
     public void stop() throws InterruptedException {
-        while(speedKmH != 0){
+        while (speedKmH != 0) {
             System.out.println("Car is slowing down");
-            speedKmH -= (int) LOSE_SPEED_PER_SECOND;
-            sleep(LOSE_SPEED_PER_SECOND);
+            speedKmH -= SPEED_PER_SECOND ;
+            sleep(1000);
 
         }
         System.out.println(getClass() + " stopped, the current speed is: " + getSpeedKmH());
--- src/zadanie6/Vehicle.java
+++ src/zadanie6/Vehicle.java
@@ -1,7 +1,7 @@
 package zadanie6;
 
 public interface Vehicle {
-    public void drive();
+    public void drive() throws InterruptedException;
     public void stop() throws InterruptedException;
     public void tank();
 }
